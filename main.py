f5a96913a256a0f8eed35088f5aabe20af1744c7
Rithik Kumar Singh <rksbugatti@gmal.com>
2024-11-21 23:55:13 +0530

    added some commands

--- main.py
+++ main.py
@@ -15,4 +15,25 @@ with open ("sample.html",'r') as f:
     html_doc=f.read()
 
 soup=BeautifulSoup(html_doc,"html.parser")
-print(soup.prettify())
+# print(soup.prettify())
+
+# print(soup.title) #output: <title>Rithik_Title</title>
+
+# print(soup.title.string) #output: Rithik_Title
+
+# print(soup.div) #gives first div
+
+# print(soup.find_all("div")) #gives all divs
+# print(soup.find_all("div")[1]) #gives first div
+
+# for link in soup.find_all("a"):
+#     print(link.get("href"))
+# prints all the links in href attribute
+
+# s=soup.find(id="link3")
+# print(s.get("href"))
+#  to get particular id
+
+# for link in soup.find_all("a"):
+#     print(link.get_text())
+# prints the text inside the tags. work same as link.string
