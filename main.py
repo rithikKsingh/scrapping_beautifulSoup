45aa9b3f5a43b0956f73772fddb287a98f133aa9
Rithik Kumar Singh <rksbugatti@gmal.com>
2024-11-22 00:47:21 +0530

    tut end

--- main.py
+++ main.py
@@ -37,3 +37,62 @@ soup=BeautifulSoup(html_doc,"html.parser")
 # for link in soup.find_all("a"):
 #     print(link.get_text())
 # prints the text inside the tags. work same as link.string
+
+# print(soup.select("div.italic")) #gives a list of all the divs with class italic
+
+# print(soup.select("span#italic")) #gives a list of all the spans with class italic
+    
+# print(soup.div.get("class")) # returns the list of class names of the first div. If first div doesn't contain any classes, it shows none
+
+# print(soup.span.get("id")) #returns the id of first span
+
+# print(soup.find(class_="italic"))  #class is a reserved keyword, so use class_. it returns the first tag with class attribute
+# print(soup.find_all(class_="italic")) # returns a list of all the tags with class attribute
+# print(soup.find(id="firstSpan"))  #returns first tag with id 
+
+# for child in soup.find(class_="container").children:
+#     print(child)
+#prints all the childs of the particular elements
+
+# for parent in soup.find(class_="box").parents:
+#     print(parent)
+#prints all the parents of the element
+
+
+# here it only modifies the output
+# cont=soup.find(class_="container")
+# cont.name="span"
+# cont["class"]="myClass class2"
+# cont.string="I am a string"
+# print(cont)
+
+
+
+# adding a htnl tag
+# ulTag=soup.new_tag("ul")
+
+# liTag=soup.new_tag("li")
+# liTag.string="First point"
+# ulTag.append(liTag)
+
+# liTag=soup.new_tag("li")
+# liTag.string="Sec point"
+# ulTag.append(liTag)
+
+# soup.html.body.insert(0,ulTag) #here we are adding tag to the soup object containing html, not the html file itself
+# with open("modified.html","w") as f:
+#     f.write(str(soup))  #here we need to convert the soup object into a string otherwise it will show error
+
+
+
+# returns true or false based on whether the tag has that attribute or not 
+# cont = soup.find(class_="container")
+# print(cont.has_attr("contenteditable"))
+
+
+# prints all the elements which as attribute class but not id
+# def has_class_but_not_id(tag):
+#     return tag.has_attr("class") and not tag.has_attr("id")
+
+# results=soup.find_all(has_class_but_not_id)
+# print(results)
